ba7a0b8443a383f6d4c6a2ea55242a98ade73c2d
dbrizov <d.b.rizov@gmail.com>
2023-10-11 22:36:42 +0300

    student_profiles.py update

--- source/student_profiles.py
+++ source/student_profiles.py
@@ -13,7 +13,7 @@ REGISTER_FILE_PATH = f"{CURRENT_DIRECTORY}/{REGISTER_FILE_NAME}"
 def get_column_index(column):
     # 26 number system where [A...Z] is mapped to [1...26]
     sum = 0
-    for idx in reversed(range(len(column))):
+    for idx in reversed(range(0, len(column))):
         sum += (ord(column[idx]) - ord("A") + 1) * math.pow(26, len(column) - idx - 1)
 
     return int(sum - 1)  # the index is the decimal value minus 1
