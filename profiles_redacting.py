4e72b6ba4ba1454545aac056409ad2f65c6d32c0
flare67 <viktorkrastev1@gmail.com>
2024-11-01 12:17:14 +0200

    pip8

--- profiles_redacting.py
+++ profiles_redacting.py
@@ -1,8 +1,10 @@
 import os
 import docx
 
-CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
-OUTPUT_DIRECTORY = f"{CURRENT_DIRECTORY}/student_profiles_redacted" # The folder that stores the files (decide based on your need)
+CURRENT_DIRECTORY = os.path.dirname(
+    os.path.realpath(__file__)).replace("\\", "/")
+# The folder that stores the files (decide based on your need)
+OUTPUT_DIRECTORY = f"{CURRENT_DIRECTORY}/student_profiles_redacted"
 
 # Columns to remove (in the future you can change them just recplace with the title needed)
 titles_to_remove = {
@@ -10,9 +12,10 @@ titles_to_remove = {
     "Научил/а за ABLE Mentor от?"
 }
 
+
 def remove_specified_columns(doc_path):
     doc = docx.Document(doc_path)
-    
+
     # Loop trough the tables to get the ones in need of modification
     for table in doc.tables:
         rows_to_delete = []
@@ -20,12 +23,13 @@ def remove_specified_columns(doc_path):
             cell_text = row.cells[0].text.strip()
             if cell_text in titles_to_remove:
                 rows_to_delete.append(i)
-        
+
         # Delete the rows
         for row_idx in reversed(rows_to_delete):
             table._tbl.remove(table.rows[row_idx]._tr)
 
     doc.save(doc_path)
+
 
 def process_all_documents():
     for file_name in os.listdir(OUTPUT_DIRECTORY):
@@ -33,6 +37,6 @@ def process_all_documents():
             file_path = os.path.join(OUTPUT_DIRECTORY, file_name)
             remove_specified_columns(file_path)
 
+
 if __name__ == "__main__":
     process_all_documents()
-
