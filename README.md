729f4b14574abb7597e86121fd26639ba0ce26a2
dbrizov <d.b.rizov@gmail.com>
2024-11-02 00:01:24 +0200

    - Update .gitignore - Update README.md - Code refactoring

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ## Prerequisites
 - matplotlib==3.7.1
 - numpy==1.24.2
-- python-docx==0.8.11
+- python-docx==0.8.1
+- docx2pdf==0.1.8
 - aiosmtpd==1.4.4.post2
 - pandas==2.2.2
 - XlsxWriter==3.2.0
--- source/convert_to_pdf.py
+++ source/convert_to_pdf.py
@@ -1,24 +1,26 @@
 import os
 from docx2pdf import convert
 
-#You will have to download the docx2pdf lib with this command in the cmd ( pip install python-docx docx2pdf  ) possibly as an administrator too
+# You will have to download the docx2pdf lib with this command in the cmd ( pip install python-docx docx2pdf  ) possibly as an administrator too
 
 CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
-OUTPUT_DIRECTORY = f"{CURRENT_DIRECTORY}/student_profiles_redacted"
-PDF_OUTPUT_DIRECTORY = f"{CURRENT_DIRECTORY}/pdf_profiles"
+INPUT_DIRECTORY = f"{CURRENT_DIRECTORY}/student_profiles_redacted"
+OUTPUT_DIRECTORY = f"{CURRENT_DIRECTORY}/student_profiles_pdf"
 
-if not os.path.exists(PDF_OUTPUT_DIRECTORY):
-    os.mkdir(PDF_OUTPUT_DIRECTORY)
+if not os.path.exists(OUTPUT_DIRECTORY):
+    os.mkdir(OUTPUT_DIRECTORY)
+
 
 def convert_profiles_to_pdf():
-    for filename in os.listdir(OUTPUT_DIRECTORY):
+    for filename in os.listdir(INPUT_DIRECTORY):
         if filename.endswith(".docx"):
-            docx_path = os.path.join(OUTPUT_DIRECTORY, filename)
+            docx_path = os.path.join(INPUT_DIRECTORY, filename)
             pdf_filename = filename.replace(".docx", ".pdf")
-            pdf_path = os.path.join(PDF_OUTPUT_DIRECTORY, pdf_filename)
-            
+            pdf_path = os.path.join(OUTPUT_DIRECTORY, pdf_filename)
+
             convert(docx_path, pdf_path)
             print(f"Converted {filename} to PDF.")
+
 
 if __name__ == "__main__":
     convert_profiles_to_pdf()
